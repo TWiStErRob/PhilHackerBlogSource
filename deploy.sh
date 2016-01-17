ac800d8eb8438c3adc55185152f3c9e1f2f22e93
Matt Dupree <kmdupr33@gmail.com>
2016-01-17 15:22:39 -0500

    Updated deploy to completely wipe master branch upon every deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+rm -rf ../PhilHackerBlog
+
 # Build the project.
 hugo -d ../PhilHackerBlog
 
 cd ../PhilHackerBlog
-
+git init
 git add --all
 git commit -m "Updated site `date`"
-git push origin master
+git remote add origin https://github.com/kmdupr33/kmdupr33.github.io.git
+git push -f origin master
