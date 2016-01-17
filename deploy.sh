846f07b42f0d69ab7e1fd6a2b5b2257e589109d9
Matt Dupree <kmdupr33@gmail.com>
2016-01-17 10:14:59 -0500

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,10 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo
+hugo -d ../PhilHackerBlog
 
-# Add changes to git.
-git add -A
+cd ../PhilHackerBlog
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
+git add --all
+git commit -m "Updated site `date`"
 git push origin master
-git subtree push --prefix=public git@github.com:kmdupr33/PhilHackerBlog.git gh-pages
