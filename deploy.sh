9298784943ea544b714cd2a9d93b975672936ea1
Matt Dupree <kmdupr33@gmail.com>
2016-01-19 12:32:10 -0500

    Fixed script so that -f flag is unnecessary on push

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,12 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-rm -rf ../PhilHackerBlog
+rm -rf ../PhilHackerBlog/*
 
 # Build the project.
 hugo -d ../PhilHackerBlog
 
 cd ../PhilHackerBlog
-git init
 git add --all
 git commit -m "Updated site `date`"
-git remote add origin https://github.com/kmdupr33/kmdupr33.github.io.git
-git push -f origin master
+git push origin master
